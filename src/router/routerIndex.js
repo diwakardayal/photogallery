1b07473d2c9cecacaf27fa696e689263b7f0b9bd
diwakardayal <diwakardayal65@gmail.com>
2024-06-02 20:11:20 +0530

    add log on / route

--- src/router/routerIndex.js
+++ src/router/routerIndex.js
@@ -7,6 +7,9 @@ const userRouter = require("./userRouter");
 const imageRouter = require("./imageRoutes");
 
 router.use("/api/user", userRouter);
+router.use("/api/working", (req, res) => {
+  res.send("working");
+});
 router.use("/api/image", imageRouter);
 
 module.exports = router;
